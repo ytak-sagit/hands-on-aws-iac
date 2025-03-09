fb09b10a9644c0c8fa5124468b7326e26d876a88
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-09 13:03:04 +0000

    chore: SQSルートモジュールのtfコンフィグファイルを追加

---
+++ chapter04/env/dev/sqs/backend.tf
@@ -0,0 +1,11 @@
+// tfstate ファイルの格納先（バックエンド）の定義
+// NOTE: Terraform で AWS リソースを管理する際は、格納先を S3 とするのが一般的
+
+terraform {
+  backend "s3" {
+    // NOTE: S3 バケット名はグローバルで一意である必要がある（他のユーザとも重複できない）
+    bucket = "dev-tfstate-aws-iac-book-project"
+    key    = "sqs/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}

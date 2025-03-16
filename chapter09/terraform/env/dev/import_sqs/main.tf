51e4a03d8e70f091d12647a7863fefb7becf0555
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-16 10:30:12 +0000

    chore: `terraform state show`の出力結果を転記

--- chapter09/terraform/env/dev/import_sqs/main.tf
+++ chapter09/terraform/env/dev/import_sqs/main.tf
@@ -1,4 +1,22 @@
 # 既存リソースをインポートするための「箱」を用意
 resource "aws_sqs_queue" "import_test" {
-
+  content_based_deduplication       = false
+  deduplication_scope               = null
+  delay_seconds                     = 0
+  fifo_queue                        = false
+  fifo_throughput_limit             = null
+  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = null
+  max_message_size                  = 262144
+  message_retention_seconds         = 345600
+  name                              = "import-test"
+  name_prefix                       = null
+  # policy も表示されていたが、削除しても `No changes.` と出たので削除した
+  receive_wait_time_seconds         = 0
+  redrive_allow_policy              = null
+  redrive_policy                    = null
+  sqs_managed_sse_enabled           = true
+  tags                              = {}
+  tags_all                          = {}
+  visibility_timeout_seconds        = 10
 }

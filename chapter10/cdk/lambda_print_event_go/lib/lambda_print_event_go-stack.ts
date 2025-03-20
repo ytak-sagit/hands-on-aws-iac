de9d7a3a845c57f5ed32f223faec35a80396d1bb
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-20 09:08:58 +0000

    chore: スタック追加

--- chapter10/cdk/lambda_print_event_go/lib/lambda_print_event_go-stack.ts
+++ chapter10/cdk/lambda_print_event_go/lib/lambda_print_event_go-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+const lambdaName = "lambda_print_event_go";
+
+interface LambdaPrintEventGoStackProps extends cdk.StackProps {
+  stage: string;
+}
 
 export class LambdaPrintEventGoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: LambdaPrintEventGoStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaPrintEventGoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda 関数本体
+    new lambda.Function(this, "LambdaPrintEventZip", {
+      functionName: `${props.stage}-${lambdaName}-cdk`,
+      code: lambda.Code.fromDockerBuild("../../lambda/print_event_go", {
+        file: "Dockerfile.build",
+      }),
+      handler: "bootstrap",
+      runtime: lambda.Runtime.PROVIDED_AL2023,
+      architecture: lambda.Architecture.ARM_64,
+      // NOTE: role を指定しない場合は、必要なロールを自動的に補完
+    });
   }
 }

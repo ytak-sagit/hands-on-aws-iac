c9359de8a990ab45d73c2cf9855505bce0f11fac
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-22 07:45:02 +0000

    chore: カスタムリソースを利用するように修正

--- chapter11/cdk/custom_resource_print_event_py/lib/custom_resource_print_event_py-stack.ts
+++ chapter11/cdk/custom_resource_print_event_py/lib/custom_resource_print_event_py-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { readFileSync } from 'node:fs';
 
 export class CustomResourcePrintEventPyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const code = readFileSync('lambda/print_event/main.py', 'utf-8');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CustomResourcePrintEventPyQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const func = new lambda.Function(this, 'PrintEventLambda', {
+      functionName: 'PrintEventLambda',
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(code),
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+    });
+
+    new cdk.CustomResource(this, 'PrintEventCustomResource', {
+      serviceToken: func.functionArn,
+      serviceTimeout: cdk.Duration.seconds(15),
+      properties: {
+        Greeting: 'Hello',
+      },
+      // NOTE: role を指定しない場合は、必要なロールを自動的に補完
+    });
   }
 }

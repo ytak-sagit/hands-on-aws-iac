3566a3fc63e74cca77e388ea7c5b93c6eca3cfbd
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-22 09:22:52 +0000

    chore: カスタムリソースを利用するように修正

--- chapter11/cdk/custom_resource_s3_object/lib/custom_resource_s3_object-stack.ts
+++ chapter11/cdk/custom_resource_s3_object/lib/custom_resource_s3_object-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { readFileSync } from 'node:fs';
 
 export class CustomResourceS3ObjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const code = readFileSync('lambda/put_s3_object/main.py', 'utf-8');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CustomResourceS3ObjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const func = new lambda.Function(this, 'Function', {
+      functionName: 'CRPutS3Object',
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: 'index.lambda_handler',
+      code: lambda.Code.fromInline(code),
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(15),
+      // NOTE: role を指定しない場合は、必要なロールを自動的に補完
+    });
+
+    const bucketName = `${cdk.Stack.of(this).account}-test-bucket`;
+    const bucket = s3.Bucket.fromBucketName(this, 'Bucket', bucketName);
+    bucket.grantRead(func);
+    bucket.grantPut(func);
+    bucket.grantDelete(func);
+
+    new cdk.CustomResource(this, 'CustomResource', {
+      serviceToken: func.functionArn,
+      serviceTimeout: cdk.Duration.seconds(30),
+      properties: {
+        Bucket: bucket.bucketName,
+        Key: 'test-key-cdk',
+        Body: 'Hello, World!',
+      },
+    });
   }
 }

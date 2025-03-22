9554b690b08d6cdc084cb5861cc6b364d25deaca
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-22 08:51:13 +0000

    chore: add AWS CDK project for s3 object

---
+++ chapter11/cdk/custom_resource_s3_object/lib/custom_resource_s3_object-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CustomResourceS3ObjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CustomResourceS3ObjectQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}

0d42056f4811f65b3c36a76f949c50bc046bcb3f
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-03-22 08:36:59 +0000

    fix: handler name

--- chapter11/cdk/custom_resource_print_event_py/lambda/print_event/main.py
+++ chapter11/cdk/custom_resource_print_event_py/lambda/print_event/main.py
@@ -4,7 +4,7 @@ import cfnresponse
 # Lambda 関数ハンドラをカスタムリソース向けに修正
 # cfnresponse によって、実行成否を、受け取ったイベントの中で指定された URL に通知する
 
-def lambda_handler(event, context):
+def handler(event, context):
     try:
         print(json.dumps(event))
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {}, "CustomResourcePhysicalID")
